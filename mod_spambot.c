63fbc0c97145052799c349539b92fa9cdb139f37
Nigel Horne <njh@bandsman.co.uk>
2025-06-06 16:25:27 -0400

    Updated contact address

--- mod_spambot.c
+++ mod_spambot.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2006 Nigel Horne <njh@bandsman.co.uk>
+ *  Copyright (C) 2006 Nigel Horne <njh@nigelhorne.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
